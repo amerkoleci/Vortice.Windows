dc555f79fb9a13d15458a386d1122711e95f3807
Amer Koleci <amerkoleci@gmail.com>
2019-03-08 20:33:45 +0100

    More D3D11 bindings.

---
+++ src/Vortice.Direct3D11/ID3D11CommandList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Vortice.Direct3D;
+using Vortice.DXGI;
+
+namespace Vortice.Direct3D11
+{
+    public partial class ID3D11CommandList
+    {
+        internal ID3D11CommandList()
+            : base(IntPtr.Zero)
+        {
+
+        }
+    }
+}
---
+++ src/Vortice.Direct3D11/ID3D11DeviceContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using SharpGen.Runtime;
+using Vortice.Direct3D;
+using Vortice.DXGI;
+
+namespace Vortice.Direct3D11
+{
+    public partial class ID3D11DeviceContext
+    {
+        public ID3D11CommandList FinishCommandList(bool restoreState)
+        {
+            var result = new ID3D11CommandList();
+            FinishCommandListInternal(restoreState, result).CheckError();
+            return result;
+        }
+
+        public Result FinishCommandList(bool restoreState, ID3D11CommandList commandList)
+        {
+            return FinishCommandListInternal(restoreState, commandList);
+        }
+    }
+}
