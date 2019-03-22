85ca46611361005d5bb9bea65705993da6cbf4d5
Amer Koleci <amerkoleci@gmail.com>
2019-03-22 18:49:18 +0100

    WIP WIC, DWrite and D2D1 support.

---
+++ src/SharpD2D1/WIC/WIC.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Amer Koleci and contributors.
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+
+namespace SharpD2D1.WIC
+{
+    public static partial class WIC
+    {
+    }
+}
