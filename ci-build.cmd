5ce63bc75c3ba375ed958de888cc3a880af4ff47
Amer Koleci <amerkoleci@gmail.com>
2019-05-24 15:35:56 +0200

    zzzz

--- ci-build.cmd
+++ ci-build.cmd
@@ -1,10 +1,10 @@
 ﻿@echo off
 @setlocal
 
-dotnet restore src\Vortice.Windows.sln
+dotnet restore src/Vortice.Windows.sln
 
-dotnet pack -c Release Vortice.DirectX/Vortice.DirectX.csproj
-dotnet pack -c Release Vortice.DirectX.Direct3D11/Vortice.DirectX.Direct3D11.csproj
-dotnet pack -c Release Vortice.DirectX.Direct3D12/Vortice.DirectX.Direct3D12.csproj
-dotnet pack -c Release Vortice.DirectX.Direct2D/Vortice.DirectX.Direct2D.csproj
-dotnet pack -c Release Vortice.DirectX.ShaderCompiler/Vortice.DirectX.ShaderCompiler.csproj
+dotnet pack -c Release src/Vortice.DirectX/Vortice.DirectX.csproj
+dotnet pack -c Release src/Vortice.DirectX.Direct3D11/Vortice.DirectX.Direct3D11.csproj
+dotnet pack -c Release src/Vortice.DirectX.Direct3D12/Vortice.DirectX.Direct3D12.csproj
+dotnet pack -c Release src/Vortice.DirectX.Direct2D/Vortice.DirectX.Direct2D.csproj
+dotnet pack -c Release src/Vortice.DirectX.ShaderCompiler/Vortice.DirectX.ShaderCompiler.csproj
