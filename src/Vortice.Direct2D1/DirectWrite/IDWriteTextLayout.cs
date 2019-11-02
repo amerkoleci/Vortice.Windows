3ef793b6df4111a0c79665bfde6e708354d8f786
manju-summoner <manju_summoner@manjubox.net>
2019-11-02 21:51:02 +0900

    Add Direct2D bindings

---
+++ src/Vortice.Direct2D1/DirectWrite/IDWriteTextLayout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vortice.DirectWrite;
+
+namespace Vortice.DirectWrite
+{
+    partial class IDWriteTextLayout
+    {
+        public LineMetrics[] GetLineMetrics()
+        {
+            GetLineMetrics(null, 0, out var actualLineCount);
+            var lineMetrics = new LineMetrics[actualLineCount];
+            if (actualLineCount > 0)
+                GetLineMetrics(lineMetrics, lineMetrics.Length, out _).CheckError();
+            return lineMetrics;
+        }
+    }
+}
---
+++ src/Vortice.Direct2D1/WIC/IWICPalette.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vortice.WIC
+{
+    partial class IWICPalette
+    {
+        public int[] GetColors()
+        {
+            var colors = new int[ColorCount];
+            GetColors(colors.Length, colors, out _);
+            return colors;
+        }
+    }
+}
