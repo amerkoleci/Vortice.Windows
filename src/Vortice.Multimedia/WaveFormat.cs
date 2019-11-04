b25b35ea95b1689bb6e592d2be0bb3a225634536
Amer Koleci <amerkoleci@gmail.com>
2019-11-04 08:51:04 +0100

    [Multimedia] Fix old namespace issue.

--- src/Vortice.Multimedia/WaveFormat.cs
+++ src/Vortice.Multimedia/WaveFormat.cs
@@ -2,7 +2,6 @@
 // Distributed under the MIT license. See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
