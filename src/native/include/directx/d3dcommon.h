1d96c84d2820dc733103a4f3fad46aa04c75e639
Amer Koleci <amerkoleci@gmail.com>
2021-04-21 09:01:14 +0200

    Update headers with latest DirectX Agility SDK.

--- src/native/include/directx/d3dcommon.h
+++ src/native/include/directx/d3dcommon.h
@@ -3,7 +3,7 @@
 /* this ALWAYS GENERATED file contains the definitions for the interfaces */
 
 
- /* File created by MIDL compiler version 8.01.0624 */
+ /* File created by MIDL compiler version 8.01.0625 */
 /* @@MIDL_FILE_HEADING(  ) */
 
 
@@ -379,6 +379,7 @@ enum D3D_SRV_DIMENSION
 #define D3D_SHADER_FEATURE_RESOURCE_DESCRIPTOR_HEAP_INDEXING                              0x2000000
 #define D3D_SHADER_FEATURE_SAMPLER_DESCRIPTOR_HEAP_INDEXING                               0x4000000
 #define D3D_SHADER_FEATURE_WAVE_MMA                                                       0x8000000
+#define D3D_SHADER_FEATURE_ATOMIC_INT64_ON_DESCRIPTOR_HEAP_RESOURCE                       0x10000000
 typedef struct _D3D_SHADER_MACRO
     {
     LPCSTR Name;
