0dd4c3db7061700c2f607d8cfc7237232ee037ad
manju-summoner <manju_summoner@manjubox.net>
2021-05-31 23:40:41 +0900

    Fix IDWriteColorGlyphRunEnumerator.GetCurrentRun() to return ColorGlyphRun

---
+++ src/Vortice.Direct2D1/DirectWrite/IDWriteColorGlyphRunEnumerator.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Amer Koleci and contributors.
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+
+namespace Vortice.DirectWrite
+{
+    partial class IDWriteColorGlyphRunEnumerator
+    {
+        public ColorGlyphRun CurrentRun => GetCurrentRun();
+
+        internal unsafe ColorGlyphRun GetCurrentRun()
+        {
+            ColorGlyphRun colorGlyphRun = default;
+            ColorGlyphRun.__Native* colorGlyphRun_ = (ColorGlyphRun.__Native*)GetCurrentRun_();
+            colorGlyphRun.__MarshalFrom(ref *colorGlyphRun_);
+            return colorGlyphRun;
+        }
+    }
+}
