be1c568e8cf5b44048e2f7b794921a44cc8f1d8c
Michael Yu <miyu@dargon.io>
2022-04-03 14:21:29 -0700

    Direct2D1: ID2D1GeometrySink is IDisposable
    This is useful because ID2D1PathGeometry::Open returns a
    ID2D1GeometrySink backed by a ID2D1GeometrySinkNative and failing
    to release that native pointer will result in a memory leak.
    
    I suspect this breaks nobody as implementing a managed ID2D1GeometrySink
    already entails subclassing CallbackBase which implements IDisposable.

---
+++ src/Vortice.Direct2D1/ID2D1GeometrySink.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Amer Koleci and contributors.
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+
+namespace Vortice.Direct2D1
+{
+    public partial interface ID2D1GeometrySink : IDisposable { }
+}
