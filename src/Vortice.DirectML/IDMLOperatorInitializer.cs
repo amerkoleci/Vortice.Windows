42d152fb5470b91be34c736087dc8c545ee914fd
Aaron Sun <axiverse@gmail.com>
2022-04-26 20:47:33 -0700

    Cleanup usings and remove unused partial interfaces.

--- src/Vortice.DirectML/IDMLOperatorInitializer.cs
+++ src/Vortice.DirectML/IDMLOperatorInitializer.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright © Aaron Sun, Amer Koleci, and Contributors.
 // Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Vortice.DirectML;
 
 public partial class IDMLOperatorInitializer
