c5ef371289020a3cd170899ff078dcaea8d36354
Aaron Sun <axiverse@gmail.com>
2022-04-29 23:15:25 -0700

    DML: Explicit feature check support, optional array params, docs, etc. (#271)
    * Explicit feature check support, optional array params, docs, etc.
    
    * Update changelog for DirectML / 2.2.0 May release.

--- src/Vortice.DirectML/IDMLDevice1.cs
+++ src/Vortice.DirectML/IDMLDevice1.cs
@@ -5,7 +5,18 @@ namespace Vortice.DirectML;
 
 public partial class IDMLDevice1
 {
-    /// <include file="Documentation.xml" path="/comments/comment[@id='IDMLDevice1::CompileGraph']/*" />
+    /// <summary>
+    /// Compiles a graph of DirectML operators into an object that can be dispatched to the GPU.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// See Microsoft Docs:
+    /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/directml/nf-directml-idmldevice1-compilegraph"/>
+    /// </para>
+    /// </remarks>
+    /// <param name="graphDescription">A description of the graph to compile.</param>
+    /// <param name="executionFlags">Any flags to control the execution of this operator.</param>
+    /// <returns></returns>
     public IDMLCompiledOperator CompileGraph(GraphDescription graphDescription, ExecutionFlags executionFlags)
     {
         CompileGraph(ref graphDescription, executionFlags, typeof(IDMLCompiledOperator).GUID, out IntPtr nativePtr).CheckError();
