741d94425fd89b811f8160566f7b5037ea568578
Aaron Sun <axiverse@gmail.com>
2022-05-02 17:45:34 -0700

    Correct capitalization for YX

---
+++ src/Vortice.DirectML/ElementWiseAtanYXOperatorDescription.cs
@@ -0,0 +1,58 @@
+// Copyright © Aaron Sun, Amer Koleci, and Contributors.
+// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
+
+namespace Vortice.DirectML;
+
+public partial struct ElementWiseAtanYXOperatorDescription : IOperatorDescription, IOperatorDescriptionMarshal
+{
+    /// <summary>
+    /// Gets the type of operator description.
+    /// </summary>
+    public OperatorType OperatorType => OperatorType.ElementWiseAtanYX;
+
+    /// <include file="Documentation.xml" path="/comments/comment[@id='DML_ELEMENT_WISE_ATAN_YX_OPERATOR_DESC::ATensor']/*" />
+    public TensorDescription ATensor { get; set; }
+
+    /// <include file="Documentation.xml" path="/comments/comment[@id='DML_ELEMENT_WISE_ATAN_YX_OPERATOR_DESC::BTensor']/*" />
+    public TensorDescription BTensor { get; set; }
+
+    /// <include file="Documentation.xml" path="/comments/comment[@id='DML_ELEMENT_WISE_ATAN_YX_OPERATOR_DESC::OutputTensor']/*" />
+    public TensorDescription OutputTensor { get; set; }
+
+    #region Marshal
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    internal struct __Native
+    {
+        public IntPtr ATensor;
+        public IntPtr BTensor;
+        public IntPtr OutputTensor;
+    }
+
+    unsafe IntPtr IOperatorDescriptionMarshal.__MarshalAlloc()
+    {
+        __Native* @ref = UnsafeUtilities.Alloc<__Native>();
+
+        @ref->ATensor = ATensor.__MarshalAlloc();
+        @ref->BTensor = BTensor.__MarshalAlloc();
+        @ref->OutputTensor = OutputTensor.__MarshalAlloc();
+
+        return new(@ref);
+    }
+
+    unsafe void IOperatorDescriptionMarshal.__MarshalFree(ref IntPtr pDesc)
+    {
+        var @ref = (__Native*)pDesc;
+
+        ATensor.__MarshalFree(ref @ref->ATensor);
+        BTensor.__MarshalFree(ref @ref->BTensor);
+        OutputTensor.__MarshalFree(ref @ref->OutputTensor);
+
+        UnsafeUtilities.Free(@ref);
+    }
+    #endregion
+
+    public static implicit operator OperatorDescription(ElementWiseAtanYXOperatorDescription description)
+    {
+        return new(description);
+    }
+}
