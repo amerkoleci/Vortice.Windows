3b0b3da07240f566dbc267083c91c3fa1407851c
Aaron Sun <axiverse@gmail.com>
2022-06-07 15:23:38 -0700

    Inline constructor for scalebias

---
+++ src/Vortice.DirectML/ScaleBias.cs
@@ -0,0 +1,15 @@
+﻿// Copyright © Amer Koleci and Contributors.
+// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vortice.DirectML;
+public partial struct ScaleBias
+{
+    public ScaleBias(float scale = 1.0f, float bias = 0.0f)
+    {
+
+    }
+}
