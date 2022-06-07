dd96b4fdf8a6be6284ee5355f2b6d44b95e2f248
Aaron Sun <axiverse@gmail.com>
2022-06-07 15:24:50 -0700

    Format scalebias

--- src/Vortice.DirectML/ScaleBias.cs
+++ src/Vortice.DirectML/ScaleBias.cs
@@ -1,15 +1,18 @@
-﻿// Copyright © Amer Koleci and Contributors.
+﻿// Copyright © Aaron Sun, Amer Koleci, and Contributors.
 // Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Vortice.DirectML;
+
 public partial struct ScaleBias
 {
+    /// <summary>
+    /// Construts a <see cref="ScaleBias"/> with the given scale and bias.
+    /// </summary>
+    /// <param name="scale"></param>
+    /// <param name="bias"></param>
     public ScaleBias(float scale = 1.0f, float bias = 0.0f)
     {
-
+        Scale = scale;
+        Bias = bias;
     }
 }
