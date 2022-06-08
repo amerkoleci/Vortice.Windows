3f970e35d99c3d2f1690c08efb1f2d5396d41298
Aaron Sun <axiverse@gmail.com>
2022-06-07 22:27:26 -0700

    Inline constructor for scalebias (#305)
    * Inline constructor for scalebias
    
    * Format scalebias
    
    * Inline constructor size2d as well.

---
+++ src/Vortice.DirectML/ScaleBias.cs
@@ -0,0 +1,18 @@
+﻿// Copyright © Aaron Sun, Amer Koleci, and Contributors.
+// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
+
+namespace Vortice.DirectML;
+
+public partial struct ScaleBias
+{
+    /// <summary>
+    /// Construts a <see cref="ScaleBias"/> with the given scale and bias.
+    /// </summary>
+    /// <param name="scale"></param>
+    /// <param name="bias"></param>
+    public ScaleBias(float scale = 1.0f, float bias = 0.0f)
+    {
+        Scale = scale;
+        Bias = bias;
+    }
+}
