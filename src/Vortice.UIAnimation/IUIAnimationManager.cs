a2de96052e827c3d0281b3c2beec83bcc3a661a1
Amer Koleci <amerkoleci@gmail.com>
2022-07-04 11:18:22 +0200

    Initial UIAnimation bindings.

---
+++ src/Vortice.UIAnimation/IUIAnimationManager.cs
@@ -0,0 +1,10 @@
+// Copyright © Amer Koleci and Contributors.
+// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
+
+using System.Numerics;
+
+namespace Vortice.UIAnimation;
+
+public partial class IUIAnimationManager
+{
+}
