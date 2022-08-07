70cc6c4c2a5794796371b97003366d62ae241877
Gavin-Williams <deadstack@outlook.com>
2022-08-08 00:31:30 +1000

    Add XAudio2 sample 'HelloXAudio2' (#341)
    * Update AudioBuffer.cs
    
    Fixed a bug in the static Create method where memory allocation was being provided data length when it requires size in bytes. Because the data is generic, sizeInBytes is the correct value to provide, and it is already calculated in the method.
    
    * Add XAudio2 sample 'HelloXAudio2'
    
    A minimalist sample demonstrating how to initialize XAudio2, load a wave file into an AudioBuffer and play it.

---
+++ HelloXAudio2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Copyright © Amer Koleci and Contributors.
+// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
+
+using Vortice.Multimedia;
+using Vortice.XAudio2;
+
+namespace HelloXAudio2;
+
+public static class Program
+{
+    static async Task Main()
+    {
+        // Create the XAudio2 engine.
+        IXAudio2 audio = XAudio2.XAudio2Create(ProcessorSpecifier.DefaultProcessor);
+
+        // The engine is already in the started state when initialized.
+        // In other cases, the following call will start the audio engine.
+        // XAudio.StartEngine();
+
+        // Create mastering voice.
+        IXAudio2MasteringVoice masterVoice = audio.CreateMasteringVoice();
+
+        // Define wave format.
+        WaveFormatExtensible format = new(96000, 24, 2);
+
+        // Create source voice for buffer submission.
+        IXAudio2SourceVoice voice = audio.CreateSourceVoice(format, false);
+
+        // Load sound effect into audio buffer.
+        string filePath = "Assets\\SpaceVehicleFlyby.wav";
+        using Stream stream = new MemoryStream(File.ReadAllBytes(filePath));
+        SoundStream soundStream = new(stream);
+        AudioBuffer effectBuffer = new(soundStream.ToDataStream());
+
+        // Play buffer.
+        voice.SubmitSourceBuffer(effectBuffer, null);
+        voice.Start(0);
+
+        Console.SetCursorPosition(0, 0);
+        Console.WriteLine("Playing sound effect 'SpaceVehicleFlyby.wav'");
+
+        // XAudio2 is a non-blocking API, so wait for the buffer to finish
+        // playing before allowing the program to complete.
+        bool isPlaying = true;
+        while (isPlaying)
+        {
+            isPlaying = voice.State.BuffersQueued != 0;
+            Console.Write('.');
+            await Task.Delay(100);
+        }
+    }
+}
