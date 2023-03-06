162e61ab65c55bf5d542af04f51d435e95f3c0e0
Amer Koleci <amerkoleci@gmail.com>
2023-03-06 15:56:15 +0100

    FIX: Compilation issues.

--- src/Vortice.WinUI/ISwapChainPanelNative.cs
+++ src/Vortice.WinUI/ISwapChainPanelNative.cs
@@ -6,7 +6,7 @@ using Vortice.DXGI;
 namespace Vortice.WinUI;
 
 [Guid("63aad0b8-7c24-40ff-85a8-640d944cc325")]
-public partial  class ISwapChainPanelNative : ComObject
+public unsafe partial class ISwapChainPanelNative : ComObject
 {
     public ISwapChainPanelNative(IntPtr nativePtr) : base(nativePtr)
     {
@@ -19,7 +19,7 @@ public partial  class ISwapChainPanelNative : ComObject
         set => SetSwapChain(value).CheckError();
     }
 
-    public  Result SetSwapChain(IDXGISwapChain swapChain)
+    public Result SetSwapChain(IDXGISwapChain swapChain)
     {
         IntPtr swapChain_ = MarshallingHelpers.ToCallbackPtr<IDXGISwapChain>(swapChain);
         Result result = ((delegate* unmanaged[Stdcall]<IntPtr, void*, int>)this[3])(NativePointer, (void*)swapChain_);
