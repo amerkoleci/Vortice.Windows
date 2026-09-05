917f5c29362976959e0aa9bc625d33d376c19208
An Long <aisk1988@gmail.com>
2026-09-05 21:10:25 +0900

    [ENH] DirectML: Expose ActivationSwish operator

---
+++ src/Vortice.DirectML/ActivationSwishOperatorDescription.cs
@@ -0,0 +1,60 @@
+// Copyright © Aaron Sun, Amer Koleci, and Contributors.
+// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
+
+namespace Vortice.DirectML;
+
+public partial struct ActivationSwishOperatorDescription : IOperatorDescription, IOperatorDescriptionMarshal
+{
+    /// <summary>
+    /// Gets the type of operator description.
+    /// </summary>
+    public OperatorType OperatorType => OperatorType.ActivationSwish;
+
+    /// <include file="Documentation.xml" path="/comments/comment[@id='DML_ACTIVATION_SWISH_OPERATOR_DESC::InputTensor']/*" />
+    public TensorDescription InputTensor { get; set; }
+
+    /// <include file="Documentation.xml" path="/comments/comment[@id='DML_ACTIVATION_SWISH_OPERATOR_DESC::OutputTensor']/*" />
+    public TensorDescription OutputTensor { get; set; }
+
+    /// <include file="Documentation.xml" path="/comments/comment[@id='DML_ACTIVATION_SWISH_OPERATOR_DESC::SigmoidInputScale']/*" />
+    public float SigmoidInputScale { get; set; }
+
+    /// <inheritdoc></inheritdoc>/>
+    public override string ToString() => $"ActivationSwish: SigmoidInputScale={SigmoidInputScale}";
+
+    #region Marshal
+    [StructLayout(LayoutKind.Sequential, Pack = 0)]
+    internal struct __Native
+    {
+        public IntPtr InputTensor;
+        public IntPtr OutputTensor;
+        public float SigmoidInputScale;
+    }
+
+    unsafe IntPtr IOperatorDescriptionMarshal.__MarshalAlloc()
+    {
+        __Native* @ref = UnsafeUtilities.Alloc<__Native>();
+
+        @ref->InputTensor = InputTensor.__MarshalAlloc();
+        @ref->OutputTensor = OutputTensor.__MarshalAlloc();
+        @ref->SigmoidInputScale = SigmoidInputScale;
+
+        return new(@ref);
+    }
+
+    unsafe void IOperatorDescriptionMarshal.__MarshalFree(ref IntPtr pDesc)
+    {
+        var @ref = (__Native*)pDesc;
+
+        InputTensor.__MarshalFree(ref @ref->InputTensor);
+        OutputTensor.__MarshalFree(ref @ref->OutputTensor);
+
+        UnsafeUtilities.Free(@ref);
+    }
+    #endregion
+
+    public static implicit operator OperatorDescription(ActivationSwishOperatorDescription description)
+    {
+        return new(description);
+    }
+}
